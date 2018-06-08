2b1d824f1fd141b7d5e21e4bb3fc298b781a9808
Herbert <hdowalil@gmail.com>
2018-06-08 20:23:47 +0200

    final translation of self-documentation code samples to c#

---
+++ TrainingClean/SelfDocumentation.UnitTests/Lab01ExcerciseSolutionTrainerSpaceStationTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SelfDocumentation.Lab01.Excercise.Solution.Trainer;
+
+namespace SelfDocumentation.UnitTests
+{
+    [TestClass]
+    public class Lab01ExcerciseSolutionTrainerSpaceStationTest
+    {
+        [DataRow("90,20;98,22", 2000, 2000.0, Alert.Green)]
+
+        [DataRow("90,20;98,60", 2000, 2000.0, Alert.Yellow)]
+        [DataRow("12,20;98,22", 2000, 2000.0, Alert.Yellow)]
+        [DataRow("93,20;98,22", 500, 2000.0, Alert.Yellow)]
+        [DataRow("93,20;98,22", 2000, 430.0, Alert.Yellow)]
+
+        [DataRow("90,20;98,75", 2000, 2000.0, Alert.Red)]
+        [DataRow("3,20;98,22", 2000, 2000.0, Alert.Red)]
+        [DataRow("93,20;98,22", 23, 2000.0, Alert.Red)]
+        [DataRow("93,20;98,22", 2000, 12.0, Alert.Red)]
+
+        [DataTestMethod]
+        public void testAlert(String elements, long oxygen, double battery, Alert expectedAlert)
+        {
+            SpaceStation testObject = new SpaceStation();
+            parseElements(elements,testObject.Elements);
+            testObject.Oxygen = oxygen;
+            testObject.BatteryAmpereHours = battery;
+
+            testObject.logStatus();
+
+            Assert.AreEqual(expectedAlert, testObject.check4Problem());
+        }
+
+        private void parseElements(String input, ICollection<StructuralElement> result)
+        {
+
+            string[] elements = input.Split(";");
+            foreach (String s in elements)
+            {
+                string[] content = s.Split(",");
+                int structuralIntegrity = int.Parse(content[0]);
+                int temperatureInCelsius = int.Parse(content[1]);
+                result.Add(new StructuralElement(structuralIntegrity, temperatureInCelsius));
+            }
+        }
+
+    }
+}
---
+++ TrainingClean/SelfDocumentation/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyDescription("These are the code samples for Self-Documentation. This to demonstrate how an assembly can be self describing it´s own purpose")]
