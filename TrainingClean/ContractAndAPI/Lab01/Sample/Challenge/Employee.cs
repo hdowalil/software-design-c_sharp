8345a3e5e35e3e46481b20c297071f079bb3012b
Herbert <hdowalil@gmail.com>
2018-07-11 09:31:44 +0200

    Contract Lab 01 Sample

---
+++ TrainingClean/ContractAndAPI/Lab01/Sample/Challenge/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContractAndAPI.Lab01.Sample.Challenge
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+
+        public Employee(String name)
+        {
+            this.Name = name;
+        }
+
+        public Employee ChangeName (String newName)
+        {
+            return new Employee(newName);
+        }
+
+    }
+}
---
+++ TrainingClean/ContractAndAPI/Lab01/Sample/Solution/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace ContractAndAPI.Lab01.Sample.Solution
+{
+    /// <summary>
+    /// Immutable Employee
+    /// </summary>
+    class Employee
+    {
+        public readonly string Name;
+
+        public Employee(String name)
+        {
+            Contract.Requires(name != null);
+            Contract.Requires(name.Trim().Length > 0);
+
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// Will return a new Employee and NOT alter current instance!
+        /// So assign return value!
+        /// </summary>
+        /// <param name="newName">The new Employee´s object instance name</param>
+        /// <returns></returns>
+        public Employee ChangeName(String newName)
+        {
+            Contract.Requires(newName != null);
+            Contract.Requires(newName.Trim().Length > 0);
+
+            Employee result = new Employee(newName);
+
+            Contract.Ensures(result != null);
+
+            return result;
+        }
+
+
+    }
+}
