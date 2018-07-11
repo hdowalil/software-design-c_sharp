feda10604a444f24cab284f61f1be2507a4d9760
Herbert <hdowalil@gmail.com>
2018-07-11 11:24:48 +0200

    finished this excercise contract-and-api

---
+++ TrainingClean/ContractAndAPI/Lab01/Excercise/Solution/Trainer/InputAndOutput.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace ContractAndAPI.Lab01.Excercise.Solution.Trainer
+{
+    public class InputAndOutput
+    {
+        public static string ChooseFromList(int from, int to, params string[] options)
+        {
+            Contract.Requires(options != null);
+            Contract.Requires<ArgumentException>(from >= 0);
+            Contract.Requires<ArgumentOutOfRangeException>(options.Length < to);
+
+            int range = to - from;
+            int chosen = -1;
+
+            do
+            {
+                Console.WriteLine("Please choose:");
+                for (int i = 0; i < range; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {options[from + i]}");
+                }
+
+                String input = null;
+                do
+                {
+                    input = Console.ReadLine();
+
+                } while (!int.TryParse(input, out chosen));
+
+            } while (chosen <= 0 || chosen > range);
+
+            string result = options[chosen + from - 1];
+
+            Contract.Ensures(result != null);
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            string chosen = ChooseFromList(2, 4, "Apple", "Banana", "Strawberry", "PineApple");
+            Console.WriteLine(chosen);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TrainingClean/ContractAndAPI/Lab01/Excercise/Solution/Your/InputAndOutput.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContractAndAPI.Lab01.Excercise.Solution.Your
+{
+    public class InputAndOutput
+    {
+        public static string ChooseFromList(int from, int to, params string[] options)
+        {
+            // Improve this by making use of pre- and postcondition checks using System.Diagnostics.Contracts
+
+            int range = to - from;
+            int chosen = -1;
+
+            do
+            {
+                Console.WriteLine("Please choose:");
+                for (int i = 0; i < range; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {options[from + i]}");
+                }
+
+                String input = null;
+                do
+                {
+                    input = Console.ReadLine();
+
+                } while (!int.TryParse(input, out chosen));
+
+            } while (chosen <= 0 || chosen > range);
+
+            return options[chosen + from - 1];
+        }
+
+        public static void Main(string[] args)
+        {
+            string chosen = ChooseFromList(2, 4, "Apple", "Banana", "Strawberry", "PineApple");
+            Console.WriteLine(chosen);
+            Console.ReadLine();
+        }
+    }
+}
