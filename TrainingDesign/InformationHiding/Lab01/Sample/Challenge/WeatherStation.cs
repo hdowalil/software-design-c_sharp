8cb5f004f04e4477e927e95f9553736634b45302
Herbert <hdowalil@gmail.com>
2018-07-18 10:20:50 +0200

    translating informationhiding

---
+++ TrainingDesign/InformationHiding/Lab01/Sample/Challenge/WeatherStation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InformationHiding.Lab01.Sample.Challenge
+{
+    class WeatherStation : Dictionary<long, int>
+    {
+        public int Measure()
+        {
+
+            // now let´s assume this is a very complex thing to do:
+            Random rnd = new Random();
+            int temperature = rnd.Next(100);
+
+            return temperature;
+        }
+
+        public int MeasureAndLog()
+        {
+
+            int temperature = Measure();
+            base.Add(DateTime.Now.Ticks, temperature);
+            return temperature;
+        }
+
+    }
+}
---
+++ TrainingDesign/InformationHiding/Lab01/Sample/Solution/WeatherStation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace InformationHiding.Lab01.Sample.Solution
+{
+    class WeatherStation
+    {
+        private IDictionary<long, int> log = new Dictionary<long, int>();
+
+        public void MeasureAndLog()
+        {
+            int temperatue = Measure();
+            log.Add(DateTime.Now.Ticks, temperatue);
+        }
+
+        public int? GetMeasuredHistory(long ticks)
+        {
+            if (!log.TryGetValue(ticks, out int result))
+            {
+                return null;
+            } else
+            {
+                return result;
+            }
+        }
+
+        public IEnumerable<long> AllMeasuredHistoryLogs()
+        {
+            // the keyset is not immutable!
+            // removing a key would remove it from our Dictionary!
+            return log.Keys.ToImmutableArray();
+        }
+
+        private int Measure()
+        {
+
+            // now let´s assume this is a very complex thing to do:
+            Random rnd = new Random();
+            int temperature = rnd.Next(100);
+            return temperature;
+        }
+    }
+}
